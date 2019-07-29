4d2ea7cc29630ca74e9f4a933994b2ed6431c859
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-29 19:50:09 +0200

    updated ts definition file

--- react-condition.d.ts
+++ react-condition.d.ts
@@ -1,25 +1,30 @@
-///<reference types="react"/>
-
 export function If(props: {
   test: any;
-  children: React.ReactNode;
-}): React.ReactNode;
+  children: JSX.Element;
+}): JSX.Element;
 
 export function ElseIf(props: {
   test: any;
-  children: React.ReactNode;
-}): React.ReactNode;
+  children: JSX.Element;
+}): JSX.Element;
 
-export function Else(props: { children: React.ReactNode }): React.ReactNode;
+export function Else(props: { children: JSX.Element }): JSX.Element;
 
 export function Switch(props: {
   expression: any;
   children: Case | Default | null;
-}): React.ReactNode;
+}): JSX.Element;
 
 export function Case(props: {
   value: any;
-  children: React.ReactNode;
-}): React.ReactNode;
+  children: JSX.Element;
+}): JSX.Element;
 
-export function Default(props: { children: React.ReactNode }): React.ReactNode;
+type Default = (props: {children: JSX.Element}) => JSX.Element
+
+type Case = (props: {
+  value: any,
+  children: JSX.Element
+}) => JSX.Element
+
+export function Default(props: { children: JSX.Element }): JSX.Element;
