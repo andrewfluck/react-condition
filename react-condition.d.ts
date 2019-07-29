b197bdeada68c3aa991870a62fdf659c46a2434d
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-29 20:58:39 +0200

    element children should be able to many/one

--- react-condition.d.ts
+++ react-condition.d.ts
@@ -1,30 +1,34 @@
-export function If(props: {
-  test: any;
-  children: JSX.Element;
-}): JSX.Element;
+declare module "react-condition" {
+  export function If(props: {
+    test: any;
+    children: Children;
+  }): JSX.Element;
+  
+  export function ElseIf(props: {
+    test: any;
+    children: Children;
+  }): JSX.Element;
+  
+  export function Else(props: { children: Children }): JSX.Element;
+  
+  export function Switch(props: {
+    expression: any;
+    children: Case | Default | null;
+  }): JSX.Element;
+  
+  export function Case(props: {
+    value: any;
+    children: Children;
+  }): JSX.Element;
+  
+  type Default = (props: {children: Children}) => JSX.Element
+  
+  type Case = (props: {
+    value: any,
+    children: Children;
+  }) => JSX.Element
 
-export function ElseIf(props: {
-  test: any;
-  children: JSX.Element;
-}): JSX.Element;
-
-export function Else(props: { children: JSX.Element }): JSX.Element;
-
-export function Switch(props: {
-  expression: any;
-  children: Case | Default | null;
-}): JSX.Element;
-
-export function Case(props: {
-  value: any;
-  children: JSX.Element;
-}): JSX.Element;
-
-type Default = (props: {children: JSX.Element}) => JSX.Element
-
-type Case = (props: {
-  value: any,
-  children: JSX.Element
-}) => JSX.Element
-
-export function Default(props: { children: JSX.Element }): JSX.Element;
+  type Children = Array<JSX.Element> | JSX.Element
+  
+  export function Default(props: { children: Children }): JSX.Element;
+}
