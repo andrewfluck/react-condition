27244932646c8ed81f7643cc06c7e4973ef90483
Niputi <7137178+Niputi@users.noreply.github.com>
2019-10-08 00:37:19 +0200

    prettier formatted

--- react-condition.d.ts
+++ react-condition.d.ts
@@ -1,34 +1,22 @@
 declare module "react-condition" {
-  export function If(props: {
-    test: any;
-    children: Children;
-  }): JSX.Element;
-  
-  export function ElseIf(props: {
-    test: any;
-    children: Children;
-  }): JSX.Element;
-  
+  export function If(props: { test: any; children: Children }): JSX.Element;
+
+  export function ElseIf(props: { test: any; children: Children }): JSX.Element;
+
   export function Else(props: { children: Children }): JSX.Element;
-  
+
   export function Switch(props: {
     expression: any;
     children: Case | Default | null;
   }): JSX.Element;
-  
-  export function Case(props: {
-    value: any;
-    children: Children;
-  }): JSX.Element;
-  
-  type Default = (props: {children: Children}) => JSX.Element
-  
-  type Case = (props: {
-    value: any,
-    children: Children;
-  }) => JSX.Element
 
-  type Children = Array<JSX.Element> | JSX.Element
-  
+  export function Case(props: { value: any; children: Children }): JSX.Element;
+
+  type Default = (props: { children: Children }) => JSX.Element;
+
+  type Case = (props: { value: any; children: Children }) => JSX.Element;
+
+  type Children = Array<JSX.Element> | JSX.Element;
+
   export function Default(props: { children: Children }): JSX.Element;
 }
